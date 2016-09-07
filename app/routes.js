d79fbf3a5a46aa8313c84c195fadb7b73881f284
Kiryl Misachenka <kmisachenka@gmail.com>
2016-09-07 12:40:07 +0300

    Add implementation of restful endpoint

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,11 @@
 var core = require('./controllers/core');
+var api = require('./controllers/api');
 
 module.exports = function(app) {
 
     app.get('/', core.home);
     app.get('/top', core.top);
     app.get('/results', core.results);
-    app.get('/api/results', function(req, res) {
-        res.json({message: 'This is the API page'});
-    });
+    app.get('/api/results', api.results);
 
 };
--- app/util/db.js
+++ app/util/db.js
@@ -2,4 +2,6 @@ var loki = require('lokijs');
 
 var db = new loki('db.json');
 
+db.loadDatabase();
+
 module.exports = db;
